2fb8fb8c9b469a3a3edc82a57cb380f0b0a459a8
GerardoDev25 <GerardoDev25@gmail.com>
2021-09-03 17:26:41 -0400

    7. Validar Token - Frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,32 @@
-const socket = io();
+const urlLocal = "http://localhost:8080/api/auth";
+const url = "http://localhost:8080/api/auth/google";
+
+let user = null;
+let socket = null;
+
+// ? function that valid the JWT
+const validateJWT = async () => {
+   const token = localStorage.getItem("token") || "";
+   if (token.length <= 10) {
+      window.location = "index.html";
+      throw new Error("token no valid");
+   }
+
+   const resp = await fetch(urlLocal, {
+      headers: {
+         "x-token": token,
+      },
+   });
+
+   const { userAuth, token: tokenDB } = await resp.json();
+
+   user = userAuth;
+   localStorage.setItem("token", tokenDB);
+};
+
+const main = async () => {
+   await validateJWT();
+};
+
+main();
+// const socket = io();
